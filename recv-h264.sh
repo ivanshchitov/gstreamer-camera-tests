00cc1af229052f70e40c65880df4ee97b31765d4
Ivan Shchitov <polotenchik1992@gmail.com>
2020-06-04 14:34:40 +0300

    Add scripts to send and recieve the RAW, VP9 and H264 data streams using gst-launch

---
+++ recv-h264.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+PORT=3434
+
+fail() {
+    echo "FAIL: $@"
+    exit 1
+}
+
+usage() {
+    cat <<EOF
+
+Receive the VP9 video data from the port.
+This script can be runned only if the device has installed 'avdec_h264' plugin. 
+
+Command:
+gst-launch-1.0 -v udpsrc port=$PORT \
+caps="application/x-rtp,encoding-name=H264,payload=96" \
+! rtph264depay ! h264parse ! avdec_h264 ! autovideosink
+
+Usage:
+   $(basename $0) [OPTION]
+
+Options:
+   -p    | --port <PORT>    Port to receive video data [$PORT]
+   -h    | --help           this help
+
+EOF
+    # exit if any argument is given
+    [[ -n "$1" ]] && exit 1
+}
+
+# handle commandline options
+while [[ ${1:-} ]]; do
+    case "$1" in
+        -p | --port )
+            shift
+            PORT=$1; shift
+            ;;
+        -h | --help )
+            shift
+            usage quit
+            ;;
+        * )
+            usage quit
+            ;;
+    esac
+done
+
+gst-launch-1.0 -v udpsrc port=$PORT \
+caps="application/x-rtp,encoding-name=H264,payload=96" \
+! rtph264depay ! h264parse ! avdec_h264 ! autovideosink
---
+++ send-raw.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+
+IP_ADDRESS=127.0.0.1
+PORT=3434
+
+fail() {
+    echo "FAIL: $@"
+    exit 1
+}
+
+usage() {
+    cat <<EOF
+
+Send the RAW video data from camera by the IP-address and port.
+
+Command:
+gst-launch-1.0 -v autovideosrc device=/dev/video0 \
+! video/x-raw,width=1280,heigth=720 \
+! videoconvert ! rtpvrawpay ! udpsink host=$IP_ADDRESS port=$PORT
+
+Usage:
+   $(basename $0) [OPTION]
+
+Options:
+   -i    | --ip-address <IP_ADDRESS>   IP-address to send video data [$IP_ADDRESS]
+   -p    | --port <PORT>               Port to send video data [$PORT]
+   -h    | --help                      this help
+
+EOF
+    # exit if any argument is given
+    [[ -n "$1" ]] && exit 1
+}
+
+# handle commandline options
+while [[ ${1:-} ]]; do
+    case "$1" in
+        -i | --ip-address )
+            shift
+            IP_ADDRESS=$1; shift
+            ;;
+        -p | --port )
+            shift
+            PORT=$1; shift
+            ;;
+        -h | --help )
+            shift
+            usage quit
+            ;;
+        * )
+            usage quit
+            ;;
+    esac
+done
+
+gst-launch-1.0 -v autovideosrc device=/dev/video0 \
+! video/x-raw,width=1280,heigth=720 \
+! videoconvert ! rtpvrawpay ! udpsink host=$IP_ADDRESS port=$PORT
